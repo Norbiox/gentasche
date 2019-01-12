7a8c57c27e33b7c0324f5ff205f2d9764e4f13a8
Norbiox <norberto9411@gmail.com>
2019-01-12 11:52:22 +0100

    wrapped data generation in function

--- gen_data.py
+++ gen_data.py
@@ -24,16 +24,18 @@ if args.file is not None:
 else:
     file = 'example_gen_data.data'
 
-# generating data
-tasks = [random.randint(500000, 10000000) for _ in range(n_tasks)]
-processors = [random.randint(10, 35) * 100000 for _ in range(n_processors)]
-times_matrix = [[round(t / p, 3) for p in processors] for t in tasks]
 
-string = '\n'.join([str(n_tasks), str(n_processors)] + [
-    ' '.join(map(str, times_row)) for times_row in times_matrix
-])
+def generate_data(n_tasks, n_processors, file='example_gen_data.data'):
+    tasks = [random.randint(500000, 10000000) for _ in range(n_tasks)]
+    processors = [random.randint(10, 35) * 100000 for _ in range(n_processors)]
+    times_matrix = [[round(t / p, 3) for p in processors] for t in tasks]
+    string = '\n'.join([str(n_tasks), str(n_processors)] + [
+        ' '.join(map(str, times_row)) for times_row in times_matrix
+    ])
+    with open(file, 'w+') as f:
+        f.write(string)
+    print(f"Data saved succesfully in file: {file}")
+    
 
-with open(file, 'w+') as f:
-    f.write(string)
-
-print(f"Data saved succesfully in file: {file}")
+if __name__ == '__main__':
+    generate_data(n_tasks, n_processors, file)
