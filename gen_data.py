ca41513ac09c2be8ae2d748f488eb5677d1f4d8e
Norbiox <norberto9411@gmail.com>
2019-01-12 11:59:20 +0100

    data generator fixed

--- gen_data.py
+++ gen_data.py
@@ -3,27 +3,6 @@ import argparse
 import os
 import random
 
-# arguments parsing
-
-parser = argparse.ArgumentParser(
-    description="Generate data for description task scheduling"
-)
-parser.add_argument('tasks_number', type=int, help='number of tasks')
-parser.add_argument('processors_number', type=int, help='number of processors')
-parser.add_argument('-f', '--file', type=str, help='file to save data')
-
-args = parser.parse_args()
-n_tasks, n_processors = args.tasks_number, args.processors_number
-
-assert n_tasks > 0, "Number of tasks must be greater than 0"
-assert n_processors > 0, "Number of processors must be greater than zero"
-assert  n_tasks > n_processors, \
-    "Number of task must be greater or equal than number of processors"
-if args.file is not None:
-    file = args.file
-else:
-    file = 'example_gen_data.data'
-
 
 def generate_data(n_tasks, n_processors, file='example_gen_data.data'):
     tasks = [random.randint(500000, 10000000) for _ in range(n_tasks)]
@@ -38,4 +17,23 @@ def generate_data(n_tasks, n_processors, file='example_gen_data.data'):
     
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Generate data for description task scheduling"
+    )
+    parser.add_argument('tasks_number', type=int, help='number of tasks')
+    parser.add_argument('processors_number', type=int, help=
+                        'number of processors')
+    parser.add_argument('-f', '--file', type=str, help='file to save data')
+
+    args = parser.parse_args()
+    n_tasks, n_processors = args.tasks_number, args.processors_number
+
+    assert n_tasks > 0, "Number of tasks must be greater than 0"
+    assert n_processors > 0, "Number of processors must be greater than zero"
+    assert  n_tasks > n_processors, \
+        "Number of task must be greater or equal than number of processors"
+    if args.file is not None:
+        file = args.file
+    else:
+        file = 'example_gen_data.data'
     generate_data(n_tasks, n_processors, file)
